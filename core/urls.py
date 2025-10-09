ac16e0a7098d658062983f173a96e030276630aa
Taha-Yassine-Hadded <yessinhadded99@gmail.com>
2025-10-09 13:01:41 +0100

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,66 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Main page
+    path('', views.index, name='index'),
+    
+    # Authentication
+    path('form-login/', views.form_login, name='form_login'),
+    path('form-register/', views.form_register, name='form_register'),
+    
+    # Timeline pages
+    path('timeline/', views.timeline, name='timeline'),
+    path('timeline-event/', views.timeline_event, name='timeline_event'),
+    path('timeline-funding/', views.timeline_funding, name='timeline_funding'),
+    path('timeline-group/', views.timeline_group, name='timeline_group'),
+    path('timeline-page/', views.timeline_page, name='timeline_page'),
+    
+    # Feed
+    path('feed/', views.feed, name='feed'),
+    
+    # Groups
+    path('groups/', views.groups, name='groups'),
+    path('groups-2/', views.groups_2, name='groups_2'),
+    
+    # Pages
+    path('pages/', views.pages, name='pages'),
+    
+    # Messages
+    path('messages/', views.messages_view, name='messages'),
+    
+    # Events
+    path('event/', views.event, name='event'),
+    path('event-2/', views.event_2, name='event_2'),
+    
+    # Market/Shopping
+    path('market/', views.market, name='market'),
+    path('product-view-1/', views.product_view_1, name='product_view_1'),
+    path('product-view-2/', views.product_view_2, name='product_view_2'),
+    
+    # Videos
+    path('video/', views.video, name='video'),
+    path('video-watch/', views.video_watch, name='video_watch'),
+    
+    # Blog
+    path('blog/', views.blog, name='blog'),
+    path('blog-2/', views.blog_2, name='blog_2'),
+    path('blog-read/', views.blog_read, name='blog_read'),
+    
+    # Games
+    path('games/', views.games, name='games'),
+    
+    # Funding
+    path('funding/', views.funding, name='funding'),
+    
+    # Settings
+    path('setting/', views.setting, name='setting'),
+    path('upgrade/', views.upgrade, name='upgrade'),
+    
+    # Single page
+    path('single/', views.single, name='single'),
+    
+    # Components and code
+    path('components/', views.components, name='components'),
+    path('code/', views.code, name='code'),
+]
---
+++ core/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render
+
+# Main page
+def index(request):
+    return render(request, 'index.html')
+
+# Authentication forms
+def form_login(request):
+    return render(request, 'form-login.html')
+
+def form_register(request):
+    return render(request, 'form-register.html')
+
+# Timeline pages
+def timeline(request):
+    return render(request, 'timeline.html')
+
+def timeline_event(request):
+    return render(request, 'timeline-event.html')
+
+def timeline_funding(request):
+    return render(request, 'timeline-funding.html')
+
+def timeline_group(request):
+    return render(request, 'timeline-group.html')
+
+def timeline_page(request):
+    return render(request, 'timeline-page.html')
+
+# Feed
+def feed(request):
+    return render(request, 'feed.html')
+
+# Groups pages
+def groups(request):
+    return render(request, 'groups.html')
+
+def groups_2(request):
+    return render(request, 'groups-2.html')
+
+# Pages
+def pages(request):
+    return render(request, 'pages.html')
+
+# Messages
+def messages_view(request):
+    return render(request, 'messages.html')
+
+# Events
+def event(request):
+    return render(request, 'event.html')
+
+def event_2(request):
+    return render(request, 'event-2.html')
+
+# Market/Shopping
+def market(request):
+    return render(request, 'market.html')
+
+def product_view_1(request):
+    return render(request, 'product-view-1.html')
+
+def product_view_2(request):
+    return render(request, 'product-view-2.html')
+
+# Video pages
+def video(request):
+    return render(request, 'video.html')
+
+def video_watch(request):
+    return render(request, 'video-watch.html')
+
+# Blog pages
+def blog(request):
+    return render(request, 'blog.html')
+
+def blog_2(request):
+    return render(request, 'blog-2.html')
+
+def blog_read(request):
+    return render(request, 'blog-read.html')
+
+# Games
+def games(request):
+    return render(request, 'games.html')
+
+# Funding
+def funding(request):
+    return render(request, 'funding.html')
+
+# Settings and account
+def setting(request):
+    return render(request, 'setting.html')
+
+def upgrade(request):
+    return render(request, 'upgrade.html')
+
+# Single page (could be profile or post single)
+def single(request):
+    return render(request, 'single.html')
+
+# Components page (for UI showcase)
+def components(request):
+    return render(request, 'components.html')
+
+# Code page (for developers)
+def code(request):
+    return render(request, 'code.html')
